12cd26276fb7e8f17915d31044a6703972e96696
joway <joway.w@gmail.com>
2016-10-15 23:50:42 +0800

    update main

--- hackathon/main.py
+++ hackathon/main.py
@@ -1,10 +1,10 @@
 from hackathon.api import APIService
 from hackathon.helper import read_url_file
-from utils.helpers import gen_uuid
+from utils.helpers import gen_uuid, get_full_domain
 
 filename = 'data/url.txt'
 links = read_url_file(filename)
 
 for link in links:
-    resp = APIService.create_proj(name=gen_uuid(), entry_url=link)
+    resp = APIService.create_proj(name=get_full_domain(link), entry_url=link)
     print(resp)
--- project/admin.py
+++ project/admin.py
@@ -3,4 +3,9 @@ from django.contrib import admin
 # Register your models here.
 from project.models import Project
 
-admin.site.register(Project)
+
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('proj_id', 'name', 'entry_url', 'created_at')
+
+
+admin.site.register(Project, ProjectAdmin)
