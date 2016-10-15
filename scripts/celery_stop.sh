7234b0e0b968916406d7e03a9c928cd7196fb4f8
joway <joway.w@gmail.com>
2016-10-16 01:48:50 +0800

    scheduler celert

--- scripts/celery_stop.sh
+++ scripts/celery_stop.sh
@@ -1,2 +1,4 @@
+celery multi stopwait scheduler --pidfile="$HOME/spider/celery/%n.pid"
 celery multi stopwait fetcher --pidfile="$HOME/spider/celery/%n.pid"
 celery multi stopwait processor --pidfile="$HOME/spider/celery/%n.pid"
+celery multi stopwait pipeline --pidfile="$HOME/spider/celery/%n.pid"
